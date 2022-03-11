87de34ffc9a7e66e779e2dcfa5ff77d35efb0217
Nero <14k21096@gmail.com>
2022-03-11 19:32:07 +0300

    Uml diagram and fix annotations in 'Validation' methods

--- Lab1/logic/validation.py
+++ Lab1/logic/validation.py
@@ -1,12 +1,12 @@
 class Validation:
     @staticmethod
-    def card_valid(bank, card_number: int, pin_code: int, account: int):
+    def card_valid(bank, card_number: int, pin_code: int, account: int) -> bool:
         return bank.bank_info[account].card_number == card_number and bank.bank_info[account].pin_code == pin_code
 
     @staticmethod
-    def balance_valid(bank, account: int, amount_of_money: int):
+    def balance_valid(bank, account: int, amount_of_money: int) -> bool:
         return bank.bank_info[account].bank_account >= amount_of_money
 
     @staticmethod
-    def self_balance(human, amount_of_money: int):
+    def self_balance(human, amount_of_money: int) -> bool:
         return human.cash >= amount_of_money
